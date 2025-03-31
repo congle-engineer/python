558956c3ce9ff1d11e790fde53655d6b9f1f8958
congle-engineer <congle-engineer@gmail.com>
2025-03-31 09:38:19 +0700

    Update

--- practice.py
+++ practice.py
@@ -1,8 +1,33 @@
+import random
+
 if 5 > 2:
   print("Five is greater than two!")
 
 x = 5
-
 y = "Hello World!"
 
+print(x)
+print(y)
+
 # This is a comment
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+print(type(x))
+print(type(y))
+
+a, b, c = "Orange", "Banana", "Cherry"
+
+def myfunc():
+  print("Python is " + y)
+
+myfunc()
+
+print(random.randrange(1, 10))
+
+for x in "banana":
+  print(x)
